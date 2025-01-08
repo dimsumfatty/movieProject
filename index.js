1a5daa8f6820630748b447bd85a4b177e04244b6
Grace Tilley <gracetilley678@gmail.com>
2025-01-08 10:33:56 -0800

    updated

--- index.js
+++ index.js
@@ -3,13 +3,8 @@
 
 const API_URL = "https://www.omdbapi.com/";
 const API_KEY = "7776b763";
-
-async function onSearchChange(event){
-    const id = event.target.value;
-    renderPosts(id);
-
-}
-async function movie1() {
+let movies;
+async function movie() {
     try{
         const response = await fetch(`${API_URL}?i=tt3896198&apikey=${API_KEY}`);
         const movieData = await response.json();
@@ -34,31 +29,18 @@ function moviesHTML(movie) {
                     </div>
                 </div>`
 }  
-movie1();
 
-async function movie2() {
-    try{
-        const response = await fetch(`${API_URL}?i=tt0086190&apikey=${API_KEY}`);
-        const movieData = await response.json();
-        const movieListEl = document.querySelector('.movie__list');
-        movieListEl.innerHTML = moviesHTML(movieData);
-    }  catch (error) {
-        console.error("Error fetching movie data:", error);
-    }
+movie();
 
+/*async function onSearchChange(event){
+    const id = event.target.value;
+    renderMovies(id);
 }
 
-function moviesHTML(movie) {
-    return `<div class="movie__card">
-                    <div class="movie__card--wrapper">
-                        <img src="${movie.Poster}" alt="${movie.Title}" />
-                        <h3>Movie: ${movie.Title}</h3>
-                        <p><b>Year:</b> ${movie.Year}</p>
-                        <p><b>Rating:</b> ${movie.Rated}</p>
-                        <p><b>Runtime:</b> ${movie.Runtime}</p>
-                        <p><b>Genre:</b> ${movie.Genre}</p>
-                        <p><b>Plot:</b> ${movie.Plot}</p>
-                    </div>
-                </div>`
-}  
-movie2();
+
+async function renderMovies(id) {
+    const search = await fetch(`${API_URL}?i=tt3896198&apikey=${API_KEY}`);
+    const searchData = await search.json();
+    searchListEl.innerHTML = searchData.map(movie =>  moviesHTML(movie)).join('');
+
+}*/
